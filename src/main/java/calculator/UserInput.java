d475fdf999ff2b918e9e7e54b86bb4d75eab5bc2
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2023-03-01 22:37:53 +0900

    feat: (문자열 계산기) 입력값 첫 글자 검증 기능 구현

--- src/main/java/calculator/UserInput.java
+++ src/main/java/calculator/UserInput.java
@@ -4,7 +4,17 @@ public class UserInput {
 
     private final String[] values;
 
-    public UserInput(String values) {
-        this.values = values.split(" ");
+    public UserInput(String value) {
+        String[] inputValues = value.split(" ");
+        validateFirstValueNonNumeric(inputValues);
+        this.values = inputValues;
+    }
+
+    private void validateFirstValueNonNumeric(String[] inputValues) {
+        try {
+            Integer.parseInt(inputValues[0]);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("첫 글자는 숫자만 가능합니다.");
+        }
     }
 }
