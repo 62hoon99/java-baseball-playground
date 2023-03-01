cea338a0a6d767b3945e57fd2120382e1dd42da5
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2023-03-01 21:37:13 +0900

    feat: set collection에 대한 학습 테스트 요구사항2 구현

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -3,6 +3,8 @@ package study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -30,6 +32,17 @@ public class SetTest {
 
         //then
         assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    public void parameterizedTest(Integer num) throws Exception {
+        //given
+
+        //when
+
+        //then
+        assertThat(numbers.contains(num)).isTrue();
     }
 
 }
