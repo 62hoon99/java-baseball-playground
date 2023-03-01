8b3af795f491957b0e9dbd3c64c12863d93cfdf7
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2023-03-01 21:18:38 +0900

    feat: 학습테스트 실습 요구사항1 구현

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -10,4 +10,28 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @Test
+    public void split() throws Exception {
+        //given
+        String actual = "1,2";
+
+        //when
+        String[] split = actual.split(",");
+
+        //then
+        assertThat(split).contains("1", "2");
+    }
+
+    @Test
+    public void split2() throws Exception {
+        //given
+        String actual = "1";
+
+        //when
+        String[] split = actual.split(",");
+
+        //then
+        assertThat(split).containsExactly("1");
+    }
 }
