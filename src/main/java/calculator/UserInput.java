ca6ba244995591ac2c88de8dafde84232078936c
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2023-03-01 22:56:54 +0900

    feat: (문자열 계산기) 입력값 구성 검증 기능 구현

--- src/main/java/calculator/UserInput.java
+++ src/main/java/calculator/UserInput.java
@@ -1,5 +1,7 @@
 package calculator;
 
+import java.util.Arrays;
+
 public class UserInput {
 
     private final String[] values;
@@ -7,6 +9,7 @@ public class UserInput {
     public UserInput(String value) {
         String[] inputValues = value.split(" ");
         validateFirstValueNonNumeric(inputValues);
+        validateInvalidCharacter(inputValues);
         this.values = inputValues;
     }
 
@@ -15,6 +18,28 @@ public class UserInput {
             Integer.parseInt(inputValues[0]);
         } catch (Exception e) {
             throw new IllegalArgumentException("첫 글자는 숫자만 가능합니다.");
+        }
+    }
+
+    private void validateInvalidCharacter(String[] inputValues) {
+        boolean match = Arrays.stream(inputValues)
+                .noneMatch(this::hasInvalidCharacter);
+
+        if (!match) {
+            throw new IllegalArgumentException("숫자와 연산 문자로만 이루어져야 합니다.");
+        }
+    }
+
+    private boolean hasInvalidCharacter(String s) {
+        if (s.equals("+") || s.equals("-") || s.equals("/") || s.equals("*")) {
+            return false;
+        }
+
+        try {
+            Integer.parseInt(s);
+            return false;
+        } catch (Exception e) {
+            return true;
         }
     }
 }
