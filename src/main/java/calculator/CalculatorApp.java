300eea1bf74c87ec14f61949770a7f202f9b5b45
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2023-03-01 22:15:31 +0900

    feat: (문자열 계산기) 문자열 입력 기능 구현

---
+++ src/main/java/calculator/CalculatorApp.java
@@ -0,0 +1,9 @@
+package calculator;
+
+public class CalculatorApp {
+
+    public static void main(String[] args) {
+        View view = new View();
+        System.out.println(view.inputView());
+    }
+}
---
+++ src/main/java/calculator/View.java
@@ -0,0 +1,17 @@
+package calculator;
+
+import java.util.Scanner;
+
+public class View {
+
+    private final Scanner scanner;
+
+    public String inputView() {
+        return scanner.nextLine();
+    }
+
+    public View() {
+        this.scanner = new Scanner(System.in);
+    }
+
+}
