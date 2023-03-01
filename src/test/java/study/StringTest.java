ad19e9bb30df208da5f30f3e040d3b762b4ef483
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2023-03-01 21:29:21 +0900

    feat: 학습테스트 실습 요구사항3 구현

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,17 +1,21 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
+    @DisplayName("replace test")
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
 
     @Test
+    @DisplayName("split test")
     public void split() throws Exception {
         //given
         String actual = "1,2";
@@ -24,6 +28,7 @@ public class StringTest {
     }
 
     @Test
+    @DisplayName("split test")
     public void split2() throws Exception {
         //given
         String actual = "1";
@@ -36,6 +41,7 @@ public class StringTest {
     }
 
     @Test
+    @DisplayName("substring test")
     public void substring() throws Exception {
         //given
         String actual = "(1,2)";
@@ -46,4 +52,31 @@ public class StringTest {
         //then
         assertThat(substring).isEqualTo("1,2");
     }
+
+    @Test
+    @DisplayName("charAt test")
+    public void charAt() throws Exception {
+        //given
+        String actual = "abc";
+
+        //when
+        char b = actual.charAt(1);
+
+        //then
+        assertThat(b).isEqualTo('b');
+    }
+
+    @Test
+    @DisplayName("charAt exception test")
+    public void exception() throws Exception {
+        //given
+        String actual = "abc";
+
+        //when
+
+        //then
+        assertThatThrownBy(() -> actual.charAt(5))
+                .isInstanceOf(StringIndexOutOfBoundsException.class);
+    }
+
 }
