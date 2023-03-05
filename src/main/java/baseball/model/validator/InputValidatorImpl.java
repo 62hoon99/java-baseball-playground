f3e7d1c8787fbfee1c5525c3712974796a7c7c03
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2023-03-05 11:40:40 +0900

    feat: 입력값이 서로 다른 수인지 검증하는 기능 구현

--- src/main/java/baseball/model/validator/InputValidatorImpl.java
+++ src/main/java/baseball/model/validator/InputValidatorImpl.java
@@ -1,10 +1,14 @@
 package baseball.model.validator;
 
 import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class InputValidatorImpl implements InputValidator {
 
     private final String NATURAL_NUMBERS_MESSAGE = "각 자리는 자연수로만 이루어져야 합니다.";
+
+    private final String DISTINCT_INPUT_MESSAGE = "서로 다른 수로 이루어져야 합니다.";
 
     @Override
     public void validateNaturalNumbers(String input) {
@@ -27,7 +31,14 @@ public class InputValidatorImpl implements InputValidator {
 
     @Override
     public void validateDistinctInput(String input) {
+        String[] splitInput = splitInput(input);
 
+        Set<String> distinctValues = Arrays.stream(splitInput)
+                .collect(Collectors.toSet());
+
+        if (distinctValues.size() != splitInput.length) {
+            throw new IllegalArgumentException(DISTINCT_INPUT_MESSAGE);
+        }
     }
 
     @Override
