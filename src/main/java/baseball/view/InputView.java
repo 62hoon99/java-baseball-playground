f46ede88bc3b9d5e139aed94b8b73d4d42ce0d41
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2023-03-05 10:19:15 +0900

    feat: 숫자를 입력 받는 기능 구현

---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,16 @@
+package baseball.view;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    private final Scanner scanner;
+
+    public InputView() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public String number() {
+        return scanner.nextLine();
+    }
+}
