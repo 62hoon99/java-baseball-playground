4feefb276fd674b9545388fc15ed6438a813a62c
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2023-03-07 21:55:26 +0900

    feat: 검증 기능 단위 테스트 구현

---
+++ src/test/java/baseball/model/entity/AnswerNumberImplTest.java
@@ -0,0 +1,17 @@
+package baseball.model.entity;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AnswerNumberImplTest {
+
+    @Test
+    public void countStrikesTest() throws Exception {
+        //given
+
+        //when
+
+        //then
+    }
+}
