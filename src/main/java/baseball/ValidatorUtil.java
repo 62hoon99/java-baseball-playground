dc8f5af975770340ca2a4de1c2d1ec7c3fb44b68
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2023-03-12 16:00:18 +0900

    feat: 숫자인지 검증 기능 구현

--- src/main/java/baseball/ValidatorUtil.java
+++ src/main/java/baseball/ValidatorUtil.java
@@ -5,4 +5,13 @@ public class ValidatorUtil {
     public static boolean validRange(int num) {
         return num > 0 && num < 10;
     }
+
+    public static boolean validNum(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
 }
--- src/test/java/baseball/ValidatorUtilTest.java
+++ src/test/java/baseball/ValidatorUtilTest.java
@@ -3,14 +3,24 @@ package baseball;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static baseball.ValidatorUtil.*;
+import static org.assertj.core.api.Assertions.*;
+
 public class ValidatorUtilTest {
 
     @Test
     public void 숫자_1_9_검증() throws Exception {
         //then
-        Assertions.assertThat(ValidatorUtil.validRange(1)).isTrue();
-        Assertions.assertThat(ValidatorUtil.validRange(9)).isTrue();
-        Assertions.assertThat(ValidatorUtil.validRange(0)).isFalse();
-        Assertions.assertThat(ValidatorUtil.validRange(10)).isFalse();
+        assertThat(validRange(1)).isTrue();
+        assertThat(validRange(9)).isTrue();
+        assertThat(validRange(0)).isFalse();
+        assertThat(validRange(10)).isFalse();
+    }
+
+    @Test
+    public void 숫자인지_검증() throws Exception {
+        //then
+        assertThat(validNum("123")).isTrue();
+        assertThat(validNum("a12")).isFalse();
     }
 }
